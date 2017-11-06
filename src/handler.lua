4bd6706e97b2169dc1be82f6ed45e0a01a78ef8b
Henrique Canto Duarte <hcantoduarte@gmail.com>
2017-11-06 10:10:57 -0200

    Application key is now returned upon registration (#4)

--- src/handler.lua
+++ src/handler.lua
@@ -2,7 +2,7 @@ local BasePlugin = require "kong.plugins.base_plugin"
 local access = require "kong.plugins.mutualauthentication.access"
 local body_filter = require "kong.plugins.mutualauthentication.body_filter"
 
-local AuthPlugin = BasePlugin:extend() 
+local AuthPlugin = BasePlugin:extend()
 
 function AuthPlugin:new()
 	AuthPlugin.super.new(self, "mutualauthentication")
@@ -11,12 +11,21 @@ end
 function AuthPlugin:access(conf)
 	AuthPlugin.super.access(self)
 	access.run(conf)
-	ngx.ctx.buffer = ""	
+	ngx.ctx.buffer = ""
 end
 
 function AuthPlugin:body_filter(conf)
 	AuthPlugin.super.body_filter(self)
     body_filter.run(conf)
+end
+
+function AuthPlugin:header_filter(conf)
+    AuthPlugin.super.header_filter(self)
+    -- Removing because content length will change
+    request_uri = ngx.var.request_uri
+    if(string.match(request_uri, "registerComponent")) then
+        ngx.header["content-length"] = nil
+    end
 end
 
 return AuthPlugin
